c905f0a4781429417f434481169218b50996d887
Bashar EID <117010924+bashareid20@users.noreply.github.com>
2024-12-11 22:26:31 +0300

    Admin modeli eklendi ve gerekli yetkilendirmeler bu model üzerinden gerçekleştirildi.

---
+++ Web_Programlama _Proje/Models/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Programlama__Proje.Models
+{
+    public class Admin
+    {
+        [Key]
+        [Display(Name ="Admin ID")]
+        public int AdminID { get; set; }
+        [Required]
+        public string AdminEposta { get; set; }
+        [Required]
+        public string AdminSifre { get; set; }
+        [Required]
+        public string AdminYetki { get; set; }
+
+    }
+}
